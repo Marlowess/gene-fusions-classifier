6bf88c7988174de13b66cc440a966b7e3edf970c
franec94 <franebc94@gmail.com>
2020-01-16 17:32:00 +0100

    created run_analysis.py entry point.

--- ModelFactory.py
+++ ModelFactory.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 # from ModelOneHotAmminoacid import ModelOneHotAmminoacid
 from ModelEmbeddedLstmOneLayer import ModelEmbeddedLstmOneLayer
+from ModelEmbeddingBidirectProtein import ModelEmbeddingBidirectProtein
 
 class ModelFactory():
    
@@ -8,18 +9,27 @@ class ModelFactory():
    def getModelByName(model_name: str, params: dict) -> object:
       if model_name == 'EmbeddingBiLstmAttentionProtein':
          return ModelFactory.getEmbeddingBiLstmAttentionProtein(params)
+      
       if model_name == 'EmbeddingBiLstmAttentionDna':
          return ModelFactory.getEmbeddingBiLstmAttentionDna(params)
-      if model_name == 'OneHotEncodedLstm':
-         return ModelFactory.getOneHotEncodedLstm(params)       
-      if model_name == 'EmbeddingLstm':
-         return ModelFactory.getEmbeddingLstm(params)   
       
+      if model_name == 'OneHotEncodedLstm':
+         return ModelFactory.getOneHotEncodedLstm(params)
+
+      if model_name == 'EmbeddingLstm':
+         return ModelFactory.getEmbeddingLstm(params)
+      
+      if model_name == "ModelEmbeddingBidirectProtein":
+         return ModelFactory.getModelEmbeddingBidirectProtein(params)
       raise ValueError(f'ERROR: {model_name} is not allowed!')
    
    @staticmethod
    def getEmbeddingBiLstmAttentionProtein(params: dict):
        return None
+   
+   @staticmethod
+   def getModelEmbeddingBidirectProtein(params: dict):
+      return ModelEmbeddingBidirectProtein(params)
    
    @staticmethod
    def getEmbeddingBiLstmAttentionDna(params: dict):
