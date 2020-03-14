f52b309a98565e6e802d63eab30470664245191a
Michele <michele.damico1994@gmail.com>
2020-03-14 17:00:55 +0100

    fix spaces to 4

--- utils/preprocess_dataset_util.py
+++ utils/preprocess_dataset_util.py
@@ -14,20 +14,20 @@ def _log_info_message(message: str, logger:  logging.Logger, skip_message: bool 
     pass
 
 def _labels_text2num(labels_list: list) -> np.array:
-  """Function `_labels_text2num` takes a plain Python list as input,
-  and retrieves a `np.array` object, mapping the values within the list from characters to non-negative integers.
+    """Function `_labels_text2num` takes a plain Python list as input,
+    and retrieves a `np.array` object, mapping the values within the list from characters to non-negative integers.
 
-  Params:
-  -------
-    :labels_list: non empty Python list.
-  Return:
-  -------
-    :np.array: a numpy iterable array of non-negative integers.
-  """
+    Params:
+    -------
+        :labels_list: non empty Python list.
+    Return:
+    -------
+        :np.array: a numpy iterable array of non-negative integers.
+    """
 
-  char2int = lambda x: 0 if x == 'N' else 1
-  mapped_labels: list = list(map(char2int, labels_list))
-  return np.array(mapped_labels, dtype=np.int32)
+    char2int = lambda x: 0 if x == 'N' else 1
+    mapped_labels: list = list(map(char2int, labels_list))
+    return np.array(mapped_labels, dtype=np.int32)
 
 def _tokenize(data_samples, conf_tok_dict: dict, data_tokenizer = None) -> object:
     """Function `_tokenize` receives as input parameters two objects, which
@@ -36,12 +36,12 @@ def _tokenize(data_samples, conf_tok_dict: dict, data_tokenizer = None) -> objec
 
     Params:
     -------
-      :data_samples: iterable object, containing biological sequences.
-      :conf_tok_dict: Python dict, with the options describing how to perform tokenization task.
+        :data_samples: iterable object, containing biological sequences.
+        :conf_tok_dict: Python dict, with the options describing how to perform tokenization task.
     Return:
     -------
-      :tensor: result provided by tokenization task, if specified this result might be onehot-encoded too.
-      :data_tokenizer: `tf.keras.preprocessing.text.Tokenizer` object with the specification describing how tokenization task has been performed.
+        :tensor: result provided by tokenization task, if specified this result might be onehot-encoded too.
+        :data_tokenizer: `tf.keras.preprocessing.text.Tokenizer` object with the specification describing how tokenization task has been performed.
     """
 
 
@@ -61,35 +61,35 @@ def _tokenize(data_samples, conf_tok_dict: dict, data_tokenizer = None) -> objec
     tensor = tf.keras.preprocessing.sequence.pad_sequences(tensor, maxlen=maxlen, padding=padding)
 
     if onehot_flag is True:
-      num_classes: int = len(data_tokenizer.index_word)+1
-      tensor = tf.keras.utils.to_categorical(tensor, num_classes=num_classes)
+        num_classes: int = len(data_tokenizer.index_word)+1
+        tensor = tf.keras.utils.to_categorical(tensor, num_classes=num_classes)
 
     return tensor, data_tokenizer
 
 def preprocess_data(data: dict, conf_tok_dict: dict, main_logger: logging.Logger) -> object:
-  """Function `preprocess_data` takes as inputs two parameters which are two Python dictionary,
-  which are the former a dictionary of samples and labels for train, val, and test sets, while the latter a dictionary
-  with the specification describing how to carry out the preprocessing step.
+    """Function `preprocess_data` takes as inputs two parameters which are two Python dictionary,
+    which are the former a dictionary of samples and labels for train, val, and test sets, while the latter a dictionary
+    with the specification describing how to carry out the preprocessing step.
 
-  Params:
-  -------
-    :data: a Python dictionary within which there are as keys the train, val and test set of features samples, and as values the related iterable objects,
-    for which there are also the corresponding train, val, test labels iterable objects referring to the related keys.
-    :conf_tok_dict: a Python dictionary containing the specification that describes how to perform preprocessing phase
-  Return:
-    x_train, y_train, x_val, y_val, x_test, y_test
-  """
+    Params:
+    -------
+        :data: a Python dictionary within which there are as keys the train, val and test set of features samples, and as values the related iterable objects,
+        for which there are also the corresponding train, val, test labels iterable objects referring to the related keys.
+        :conf_tok_dict: a Python dictionary containing the specification that describes how to perform preprocessing phase
+    Return:
+        x_train, y_train, x_val, y_val, x_test, y_test
+    """
   
-  _log_info_message(f" [*] Preprocessing data...", main_logger)
-  # pprint(conf_tok_dict)
+    _log_info_message(f" [*] Preprocessing data...", main_logger)
+    # pprint(conf_tok_dict)
   
-  x_train, tokenizer = _tokenize(data['x_train'], conf_tok_dict)
-  y_train = _labels_text2num(data['y_train'])
+    x_train, tokenizer = _tokenize(data['x_train'], conf_tok_dict)
+    y_train = _labels_text2num(data['y_train'])
 
-  x_val, _ = _tokenize(data['x_val'], conf_tok_dict, tokenizer)
-  y_val = _labels_text2num(data['y_val'])
+    x_val, _ = _tokenize(data['x_val'], conf_tok_dict, tokenizer)
+    y_val = _labels_text2num(data['y_val'])
 
-  x_test, _ = _tokenize(data['x_test'], conf_tok_dict, tokenizer)
-  y_test = _labels_text2num(data['y_test'])
+    x_test, _ = _tokenize(data['x_test'], conf_tok_dict, tokenizer)
+    y_test = _labels_text2num(data['y_test'])
   
-  return x_train, y_train, x_val, y_val, x_test, y_test, tokenizer
+    return x_train, y_train, x_val, y_val, x_test, y_test, tokenizer
